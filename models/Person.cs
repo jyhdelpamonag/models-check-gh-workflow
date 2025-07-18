d4a86008573c99ed04b17c240251a0e8e07a60b3
Jyhdel Pamonag <jyhdelmaepamonag@gmail.com>
2025-07-18 16:17:15 -0400

    test

--- models/Person.cs
+++ models/Person.cs
@@ -1,6 +1,7 @@
-public class Person {
+public class PersonAgain {
     public int Id { get; set; }
     public string Name { get; set; }
     public DateTime DateOfBirth { get; set; }
     public string Email { get; set; }
+    public string PhoneNumber { get; set; }
 }

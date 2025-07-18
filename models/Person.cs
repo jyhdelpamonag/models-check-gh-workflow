da3d4bb93741b5ffae938882e1e4aff3e12e5cd4
Jyhdel Pamonag <jyhdelmaepamonag@gmail.com>
2025-07-18 16:49:41 -0400

    New Property

--- models/Person.cs
+++ models/Person.cs
@@ -2,6 +2,7 @@ public class Person {
     public int Id { get; set; }
     public string Name { get; set; }
     public DateTime DateOfBirth { get; set; }
-    public long Email { get; set; }
+    public string Email { get; set; }
+    public long NewProp { get; set; }
     public string PhoneNumber { get; set; }
 }

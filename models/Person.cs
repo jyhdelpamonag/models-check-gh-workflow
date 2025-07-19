9a497179e54cd282060519340ad051f2c31e7433
Jyhdel Pamonag <jyhdelmaepamonag@gmail.com>
2025-07-18 20:56:56 -0400

    Delete

--- models/Person.cs
+++
@@ -1,7 +0,0 @@
-public class PersonRenamed {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-}

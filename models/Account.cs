29959364e736aa297c2486ce62595d7a55de24a8
Jyhdel Pamonag <jyhdelmaepamonag@gmail.com>
2025-07-18 20:38:28 -0400

    Delete prop

--- models/Account.cs
+++
@@ -1,7 +0,0 @@
-public class AccountRenamed {
-    public int AccountId { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public bool IsActive { get; set; }
-}

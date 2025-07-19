8a8eb0a02a083df4aa2aeafe97727c9087e95081
Jyhdel Pamonag <jyhdelmaepamonag@gmail.com>
2025-07-19 18:01:52 -0400

    test

--- models/Account.cs
+++ models/Account.cs
@@ -1,5 +1,5 @@
 public class Account {
-    public DateTime AccountId { get; set; }
+    public int AccountId { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
     public DateTime CreatedAt { get; set; }

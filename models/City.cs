231ddbf50c552ac4e40a546c5c2fb822213a2ea9
Jyhdel Pamonag <jyhdelmaepamonag@gmail.com>
2025-07-26 19:47:14 -0400

    test

--- models/City.cs
+++ models/City.cs
@@ -1,5 +1,6 @@
 public class City {
     public int CityId { get; set; }
+    public string Name { get; set; }
     public string State { get; set; }
     public int Population { get; set; }
     public string? Country { get; set; }
